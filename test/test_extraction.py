0f585b7b25b50e58bd637f58468438d4e2c225c1
Joe <jli@spartaglobal.com>
2020-08-25 15:56:36 +0100

    tested to see if the lists are greater than zero and created tests folder

--- test/test_extraction.py
+++
@@ -1,9 +0,0 @@
-import boto3
-from ExtractionClass import ExtractFromS3
-
-def test_extraction():
-    extract = ExtractFromS3()
-    assert len(extract) == 0
-
-
-test_extraction()
---
+++ test_extraction.py
@@ -0,0 +1,15 @@
+import boto3
+from ExtractionClass import ExtractFromS3
+
+
+
+def test_extraction():
+    test = ExtractFromS3()
+    assert len(test.talent_csv_list) == 0
+    test.get_data()
+    assert len(test.talent_csv_list) > 0
+    assert len(test.talent_json_list) > 0
+    assert len(test.talent_txt_list) > 0
+    assert len(test.academy_csv_list) > 0
+
+test_extraction()
