33e60efc5382013641d14016d4007f2a7f97db26
Joe <jli@spartaglobal.com>
2020-08-25 16:13:01 +0100

    added comments

--- test_extraction.py
+++ test_extraction.py
@@ -3,7 +3,9 @@ from ExtractionClass import ExtractFromS3
 
 def test_extraction():
     test = ExtractFromS3()
+    # Run the get_data method to flood the lists with rows
     test.get_data()
+    # See if the length of each list is greater than zero
     assert len(test.talent_csv_list) > 0
     assert len(test.talent_json_list) > 0
     assert len(test.talent_txt_list) > 0
