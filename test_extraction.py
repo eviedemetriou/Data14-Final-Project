683859ab943c1835fda8a679126371ada1c35f0b
Joe <jli@spartaglobal.com>
2020-08-25 16:03:36 +0100

    tested to see if the lists are greater than zero, removed asserts to see if lists equal to 0 before get_data is called

--- test_extraction.py
+++ test_extraction.py
@@ -3,7 +3,6 @@ from ExtractionClass import ExtractFromS3
 
 def test_extraction():
     test = ExtractFromS3()
-    assert len(test.talent_csv_list) == 0
     test.get_data()
     assert len(test.talent_csv_list) > 0
     assert len(test.talent_json_list) > 0
