1e9f0a3925c7133954e35f5319a712771ed04a20
Joe <jli@spartaglobal.com>
2020-08-26 12:24:48 +0100

    more symbols!

--- s3_project/tests/test_phone_number.py
+++ s3_project/tests/test_phone_number.py
@@ -9,3 +9,4 @@ def test_phone_number():
     assert test_number.cleaning_phone_numbers("=44-123-4567890") == "+44 123 456 7890"
     assert test_number.cleaning_phone_numbers("+44-123-4567890") == "+44 123 456 7890"
     assert test_number.cleaning_phone_numbers("441234567890") == "+44 123 456 7890"
+    assert test_number.cleaning_phone_numbers("+-=-!4""!4123@@45!^67&890") == "+44 123 456 7890"
