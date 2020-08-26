4e8ac48a642d212c247f57bde8120a3b259d853d
Juxhen Bica <juxhen.bica@gmail.com>
2020-08-26 12:28:07 +0100

    use story + tests completed

--- s3_project/classes/applicant_info_class.py
+++ s3_project/classes/applicant_info_class.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 
 class ApplicantInfoClean(ExtractFromS3):
+
     def __init__(self):
         super().__init__()
         self.iterate()
@@ -18,8 +19,10 @@ class ApplicantInfoClean(ExtractFromS3):
             s3_object = self.s3_client.get_object(Bucket=self.bucket_name, Key=file)
             body = s3_object['Body'].read()
             object_dict = json.loads(body)
-            self.split_first_name_and_surname(file, object_dict)
-            self.time_format(object_dict)
+            if len(object_dict['name'].split(' ')) > 2:
+                self.append_file(file)
+            self.split_names(object_dict)
+            self.date_format(object_dict)
             self.boolean_values(object_dict)
             talent_json_dict[file] = object_dict
         return object_dict
@@ -27,25 +30,25 @@ class ApplicantInfoClean(ExtractFromS3):
     # This method splits name into first_name and last_name,
     # if there's more than 2 names, every name but the last goes into the first_name column,
     # and the ones with more than 2 names get appended to a text file.
-    def split_first_name_and_surname(self, file, object_dict):
+    def split_names(self, object_dict):
         name_list = object_dict['name'].split(' ')
         if len(name_list) > 2:
             object_dict['first_name'] = " ".join(name_list[:-1])
             object_dict['last_name'] = name_list[-1]
             object_dict.pop('name')
-            self.append_to_txt_file(file)
         elif len(name_list) == 2:
             object_dict['first_name'] = name_list[0]
             object_dict['last_name'] = name_list[-1]
             object_dict.pop('name')
+        return [object_dict['first_name'], object_dict['last_name']]
 
     # This method appends a text file.
-    def append_to_txt_file(self, file):
+    def append_file(self, file):
         with open("applicant_info_edgecases.txt", "a") as ai:
             ai.writelines(f"{file}\n")
 
     # This method cleans the date column
-    def time_format(self, object_dict):
+    def date_format(self, object_dict):
         date = object_dict['date']
         date = date.replace('//', '/')
         object_dict['date'] = datetime.strptime(date, '%d/%M/%Y').strftime('%Y/%M/%d')
@@ -53,19 +56,13 @@ class ApplicantInfoClean(ExtractFromS3):
 
     # This method changes result, self_dev, financial_support and geo_flex to boolean values.
     def boolean_values(self, object_dict):
-        if object_dict['result'] == 'Fail' or 'fail':
-            object_dict['result'] = False
-        elif object_dict['result'] == 'Pass' or 'pass':
-            object_dict['result'] = True
-        if object_dict['self_development'] == 'No' or 'no':
-            object_dict['self_development'] = False
-        elif object_dict['self_development'] == 'Yes' or 'yes':
-            object_dict['self_development'] = True
-        if object_dict['financial_support_self'] == 'No' or 'no':
-            object_dict['financial_support_self'] = False
-        elif object_dict['financial_support_self'] == 'Yes' or 'yes':
-            object_dict['financial_support_self'] = True
-        if object_dict['geo_flex'] == 'No' or 'no':
-            object_dict['geo_flex'] = False
-        elif object_dict['geo_flex'] == 'Yes' or 'yes':
-            object_dict['geo_flex'] = True
+        list_of_booleans = [object_dict['result'], object_dict['self_development'],
+                            object_dict['financial_support_self'], object_dict['geo_flex']]
+        list_of_booleans = list(map(lambda x: True if x == 'Yes' or x == 'Pass' else False, list_of_booleans))
+        object_dict['result'] = list_of_booleans[0]
+        object_dict['self_development'] = list_of_booleans[1]
+        object_dict['financial_support_self'] = list_of_booleans[2]
+        object_dict['geo_flex'] = list_of_booleans[3]
+        return [object_dict['result'], object_dict['self_development'], object_dict['financial_support_self'], object_dict['geo_flex']]
+
+juxhen = ApplicantInfoClean()
