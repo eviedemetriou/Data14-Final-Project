e290675d573b9fa6ac6ff4f2195032d1848df67a
Charlotte <CKings@spartaglobal.com>
2020-08-26 16:31:59 +0100

    making scores integers

--- academy_class.py
+++ academy_class.py
@@ -1,5 +1,7 @@
 import boto3
 import pandas as pd
+import datetime
+import math
 from ExtractionClass import ExtractFromS3
 
 class Academy(ExtractFromS3):
@@ -7,10 +9,12 @@ class Academy(ExtractFromS3):
     def __init__(self):
         super().__init__()
         self.df_list = []
+        self.column_names = []
         self.read_object()
         self.split_trainee_names('sparta_scores_issues.txt')
         self.split_trainer_names()
         self.rearrange()
+        self.check_scores()
 
 
     def read_object(self):
@@ -20,7 +24,8 @@ class Academy(ExtractFromS3):
                 Bucket= self.bucket_name,
                 Key= obj)
             df = pd.read_csv(s3_object['Body'])
-            df['course_name'] = obj.split('_')[0] +'_'+ obj.split('_')[1]  # Make a new column with the course name
+            course_name_split = obj.split('_')[0] +'_'+ obj.split('_')[1]
+            df['course_name'] = course_name_split.split('/')[1]  # Make a new column with the course name
             df['course_start_date'] = obj.split('_')[2].split('.')[0]   # Make a new column with the course start date
             self.df_list.append(df)
 
@@ -34,16 +39,15 @@ class Academy(ExtractFromS3):
             df['last_name'] = trainee_split.str[-1]
             new_df_list.append(df)
             for index, row in df.iterrows():
-                if len(df['first_name']) >=3:
+                if len(row['first_name']) >=2:
                     self.append_to_txt_file(row['name'])
+                    self.append_to_txt_file(row['course_name'])
         self.df_list = new_df_list
 
 
-
     def append_to_txt_file(self,file):
-        with open("sparta_scores_issues.txt", "w") as text_file:
+        with open("sparta_scores_issues.txt", "a") as text_file:
             text_file.writelines(f"{file}\n")
-
 
 
     def split_trainer_names(self):
@@ -57,6 +61,8 @@ class Academy(ExtractFromS3):
         self.df_list = new_df_list
 
 
+
+
     def rearrange(self):
         # Rearranging the order of the dataframes
         new_df_list = []
@@ -66,16 +72,28 @@ class Academy(ExtractFromS3):
             course_duration = (len(list(df.columns)) - 6)/ len(behaviours)
             course_weeks = list(range(1,int(course_duration) + 1))
 
-            column_names = ["first_name","last_name","trainer_first_name","trainer_last_name",'course_name','course_start_date']
+            self.column_names = ["first_name", "last_name", "trainer_first_name", "trainer_last_name", 'course_name',
+                            'course_start_date']
             for week in course_weeks:
                 for behaviour in behaviours:
-                    column_names.append(f'{behaviour}_W{week}')
-            df = df[column_names]
+                    self.column_names.append(f'{behaviour}_W{week}')
+            df = df[self.column_names]
             new_df_list.append(df)
         self.df_list = new_df_list
 
 
+    def check_scores(self):
+        new_df_list = []
+        for df in self.df_list:
+            for col in range(6, len(self.column_names)):
+                for score in df.iloc[:, col]:
+                    if not math.isnan(score):
+                        score = int(score)
+            new_df_list.append(df)
+        self.df_list = new_df_list
+
 test = Academy()
+#print(test.check_scores())
 print(test.df_list)
 #print(test.rearrange())
 
