b33d50d3f98d2f645a1e5ec8118876f349500f63
CWeston98 <68019159+CWeston98@users.noreply.github.com>
2020-08-26 11:21:33 +0100

    Update ExtractionClass.py

--- ExtractionClass.py
+++ ExtractionClass.py
@@ -2,22 +2,23 @@ import boto3
 
 
 class ExtractFromS3:
-    # Initialisation with lists to hold objects obtained from methods
     def __init__(self):
+    # Initialisation with lists to hold objects obtained from methods
         self.s3_client = boto3.client('s3')
         self.s3_resource = boto3.resource('s3')
         self.bucket_list = self.s3_client.list_buckets()
         self.bucket_name = 'data14-engineering-project'
         self.bucket = self.s3_resource.Bucket(self.bucket_name)
         self.contents = self.bucket.objects.all()
-        self.academy_csv_list = []  # Lists fpr storing separated objects
+        # Creating lists for storing separated file names
+        self.academy_csv_list = []
         self.talent_csv_list = []
         self.talent_json_list = []
         self.talent_txt_list = []
 
-    # Method to separate objects into lists in preparation for cleaning
     def get_data(self):
-        for obj in self.contents:  # Iterating through each object in each bucket
+        # Method to separate objects into lists in preparation for cleaning
+        for obj in self.contents:
             object_key = obj.key
             if object_key.startswith('Talent'):
                 if object_key.endswith('.csv'):  # To retrieve csv files in the Talent bucket
