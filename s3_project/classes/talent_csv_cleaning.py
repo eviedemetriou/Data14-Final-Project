be4caf90e2036ffc3064d1790964df7f7516bdb7
Joe <jli@spartaglobal.com>
2020-08-27 11:05:55 +0100

    dob testing finished

--- s3_project/classes/talent_csv_cleaning.py
+++ s3_project/classes/talent_csv_cleaning.py
@@ -2,12 +2,14 @@ from s3_project.classes.ExtractionClass import ExtractFromS3
 import pandas as pd
 import datetime
 
+
 class TalentCsv(ExtractFromS3):
     def __init__(self):
         # Inherited Extraction class, ran method to get data
         super().__init__()
         super().get_data()
         self.running_cleaner_methods()
+        self.df_talent_csv = pd.DataFrame()
 
     def running_cleaner_methods(self):
         # Iterating through list of csv's, accessing the Body to enable cleaning
@@ -16,18 +18,23 @@ class TalentCsv(ExtractFromS3):
                 Bucket=self.bucket_name,
                 Key=index)
             df = pd.read_csv(obj['Body'])
-            df['phone_number'] = df['phone_number'].apply(self.cleaning_phone_numbers)
+            df = df.drop(columns='id')
             df['first_name'] = df['name'].apply(self.splitting_first_names)
             df['last_name'] = df['name'].apply(self.splitting_last_names)
             df['gender'] = df['gender'].apply(self.formatting_gender)
-            df = df.drop(columns='id')
             df['dob'] = df['dob'].apply(self.dob_formatting)
+            df['phone_number'] = df['phone_number'].apply(self.cleaning_phone_numbers)
+            df['invited_date'] = df['invited_date'].apply(self.changing_day_type)
+            df['invitation_date'] = df['invited_date'] + '/' + df['month']
+            df['invited_by'] = df['invited_by'].replace({'Bruno Bellbrook': 'Bruno Belbrook', 'Fifi Eton': 'Fifi Etton'}, inplace=True)
+            self.df_talent_csv.append(df, ignore_index=True)
+
 
     def cleaning_phone_numbers(self, phone):
         # Takes a phone number as an argument, changes format to fit our requirements
         if type(phone) is str:
             if phone.startswith('0'):
-                phone = phone.replace('0', '44', 1)
+                phone.replace('0', '44', 1)
             phone_filter = filter(str.isdigit, phone)
             clean_phone = "".join(phone_filter)
             format_phone = clean_phone[:2] + ' ' + clean_phone[2:5] + ' ' + clean_phone[5:8] + ' ' + clean_phone[8:]
@@ -68,3 +75,9 @@ class TalentCsv(ExtractFromS3):
             return datetime_obj
         else:
             return date
+
+    def changing_day_type(self, num):
+        if type(num) is float:
+            num = str(num)
+            num = num.strip(num[-2:])
+            return num
