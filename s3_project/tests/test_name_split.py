758deef8ab17662dadce46fa192cc26041754e7e
Joe <jli@spartaglobal.com>
2020-08-27 10:36:25 +0100

    testing name split

--- s3_project/tests/test_name_split.py
+++ s3_project/tests/test_name_split.py
@@ -2,6 +2,8 @@ from s3_project.classes.talent_csv_cleaning import TalentCsv
 test = TalentCsv()
 
 def test_first_name():
+    # Test to see if the method returns the first name and any "middle" names capitalised
+    # Regardless of formatting
     assert test.splitting_first_names('john doe') == 'John'
     assert test.splitting_first_names('John Doe') == 'John'
     assert test.splitting_first_names('JOHN DOE') == 'John'
@@ -9,7 +11,10 @@ def test_first_name():
     assert test.splitting_first_names('John Van De Doe') == 'John Van De'
 
 def test_last_name():
-    assert test.splitting_first_names('john doe') == 'Doe'
-    assert test.splitting_first_names('John DOE') == 'Doe'
-    assert test.splitting_first_names('John Van Doe') == 'Doe'
-    assert test.splitting_first_names('John Van De Doe') == 'Doe'
+    # Test to see if the method returns the last name only and capitalised
+    # Regardless of formatting
+    assert test.splitting_last_names('john doe') == 'Doe'
+    assert test.splitting_last_names('John Doe') == 'Doe'
+    assert test.splitting_last_names('John DOE') == 'Doe'
+    assert test.splitting_last_names('John Van Doe') == 'Doe'
+    assert test.splitting_last_names('John Van De Doe') == 'Doe'
