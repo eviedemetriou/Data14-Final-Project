8a1a2f8a89a1df6530b73303fc104bc4f0207869
Alex <ASam@spartaglobal.com>
2020-08-27 13:58:39 +0100

    'Added format address, checking valid email and flag names'

--- s3_project/classes/talent_csv_cleaning.py
+++ s3_project/classes/talent_csv_cleaning.py
@@ -10,7 +10,6 @@ class TalentCsv(ExtractFromS3):
         self.df_talent_csv = pd.DataFrame()
         self.running_cleaner_methods()
 
-
     def running_cleaner_methods(self):
         # Iterating through list of csv's, accessing the Body to enable cleaning
         for index in self.talent_csv_list:
@@ -19,19 +18,23 @@ class TalentCsv(ExtractFromS3):
                 Key=index)
             df = pd.read_csv(obj['Body'])
             df = df.drop(columns='id')
+            df['name'].apply(self.flag_name)
+            df['email'].apply(self.email_valid)
             df['first_name'] = df['name'].apply(self.splitting_first_names)
             df['last_name'] = df['name'].apply(self.splitting_last_names)
             df['gender'] = df['gender'].apply(self.formatting_gender)
             df['dob'] = df['dob'].apply(self.dob_formatting)
+            df['address'] = df['address'].apply(self.format_address)
             df['phone_number'] = df['phone_number'].apply(self.cleaning_phone_numbers)
+            df['degree'] = df['degree'].replace({'1st': '1', '3rd': '3'})
             df = self.concat_dates(df, 'invited_date', 'month')
             df['invited_date'] = df['new_date']
             df = df.drop(columns=['month', 'new_date'])
-            df['invited_by'] = df['invited_by'].replace({'Bruno Bellbrook': 'Bruno Belbrook', 'Fifi Eton': 'Fifi Etton'})
-            df_talent_csv = self.df_talent_csv.append(df, ignore_index=True)
-            self.df_talent_csv = df_talent_csv
-        print(self.df_talent_csv[['invited_date', 'invited_by']])
-
+            df['invited_by'] = df['invited_by'].replace(
+                {'Bruno Bellbrook': 'Bruno Belbrook', 'Fifi Eton': 'Fifi Etton'})
+            df = df[['first_name', 'last_name', 'gender', 'dob', 'email', 'city', 'address', 'postcode', 'phone_number',
+                                                                        'uni', 'degree', 'invited_date', 'invited_by']]
+            self.df_talent_csv = self.df_talent_csv.append(df, ignore_index=True)
 
     def cleaning_phone_numbers(self, phone):
         # Takes a phone number as an argument, changes format to fit our requirements
@@ -65,6 +68,7 @@ class TalentCsv(ExtractFromS3):
             return name
 
     def formatting_gender(self, gender):
+        # This assigns the gender to a single letter M or F
         if type(gender) is str:
             gender = gender.title()
             return gender[0]
@@ -72,6 +76,7 @@ class TalentCsv(ExtractFromS3):
             return gender
 
     def dob_formatting(self, date):
+        # This formats the date in YYYY/MM/dd
         if type(date) is str:
             date_format = '%d/%m/%Y'
             datetime_obj = datetime.datetime.strptime(date, date_format).strftime('%Y/%m/%d')
@@ -79,23 +84,29 @@ class TalentCsv(ExtractFromS3):
         else:
             return date
 
-    def changing_day_type(self, num):
-        if type(num) is float:
-            num = str(num)
-            num = num.strip(num[-2:])
-            return num
-
     def concat_dates(self, df, day, month_year):
+        # This takes in a dataframe, with the name of two columns and returns a concatenated and formatted date
         df[day] = df[day].fillna(0)
         df[month_year] = df[month_year].fillna(0)
         df['new_date'] = df[day].astype(int).map(str) + ' ' + df[month_year].map(str)
         df['new_date'].replace({'0 0': None}, inplace=True)
         df['new_date'] = pd.to_datetime(df.new_date).dt.strftime('%Y/%m/%d')
-        # df = df.drop(columns=[day, month_year, 'new_date'])
         return df
 
-test = TalentCsv()
+    def flag_name(self, name):
+        # This flags the name if it there are more than two names
+        if len(name.split(' ')) > 2:
+            with open("monthly_applicant_names_edgecases.txt", "a") as ai:
+                ai.writelines(f"{name.title()}\n")
 
+    def format_address(self, address):
+        # This formats the addresses to lower case with each word capitalised
+        if type(address) is str:
+            return address.title()
 
-
-
+    def email_valid(self, email):
+        # Checks if the email address includes an @ symbol and the fields that don't are written to a text file
+        if type(email) is str:
+            if '@' not in email:
+                with open("monthly_applicant_emails_edgecases.txt", "a") as ai:
+                    ai.writelines(f"{email}\n")
