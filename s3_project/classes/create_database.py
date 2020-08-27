f4d1a52ee9d53880f1136db546b73d6e5bd1af76
CWeston98 <68019159+CWeston98@users.noreply.github.com>
2020-08-27 18:57:22 +0100

    cursor object working; connection to sql server is successful now

--- s3_project/classes/create_database.py
+++ s3_project/classes/create_database.py
@@ -1,12 +1,13 @@
 import pyodbc
 
+
 class ProjectDatabase:
     def __init__(self):
         self.server = '35.179.41.219'
         self.database = 'SpartaGlobal'
         self.username = 'SA'
         self.password = 'Passw0rd2018'
-        self.connection_string = "DRIVER={ODBC Driver 17 for SQL Server};"
+        self.connection_string = "DRIVER={SQL Server};"
         self.connection_string += f"SERVER={self.server};"
         self.connection_string += f"DATABASE={self.database};"
         self.connection_string += f"UID={self.username};"
@@ -18,7 +19,7 @@ class ProjectDatabase:
         return self.cursor.execute(sql_query)
 
     def create_keyless_table(self, table_name, table_dict):
-        # Creates a table with no keys
+        # Creates a table with no keys from {table_name, col1: [type, if null], col2: [type, if null]...}
         all_lines = []
         for column in table_dict:
             all_lines.append(f"{column} {table_dict[column][0]} {table_dict[column][1]}")
@@ -30,6 +31,10 @@ class ProjectDatabase:
         query += ',\n'.join(all_lines)
         query += ');'
         self._sql_query(query)
+        self.sparta.commit()
+
+    #def add_table_keys(self, table_name, p_key, f_keys):
+        #for
 
 
 new = ProjectDatabase()
@@ -37,3 +42,4 @@ new.create_keyless_table('Location', {
                                     'LocationID': ['INT', 'NOT NULL'],
                                     'LocationName': ['VARCHAR(25)', 'NOT NULL']
                                      })
+
